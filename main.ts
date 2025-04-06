79d99e9953f7932ff405fb024e8881428de464b7
divinejoshua <divineuche399@gmail.com>
2025-04-06 13:53:51 +0100

    Queried collection

---
+++ main.ts
@@ -0,0 +1,50 @@
+import Wetrocloud from "wetro-sdk";
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
+const API_SECRET = process.env.WETROCLOUD_API_SECRET || "";
+
+// Initialize Wetrocloud with your API secret
+const wetrocloud = new Wetrocloud({ apiSecret: API_SECRET });
+
+// Wrap everything in an async function
+const run = async () => {
+  try {
+    // Set the collection ID
+    const collection_id = "unique_id_123456789";
+
+    // Create a new collection
+    await wetrocloud.createCollection({ collection_id });
+
+    // Add a resource to the collection
+    await wetrocloud.insertResource({
+      collection_id: collection_id,
+      resource:
+        "https://medium.com/@wetrocloud/how-does-artificial-intelligence-work-1a1fb52e4a56",
+      // @ts-ignore
+      type: "web",
+    });
+
+    // Query the collection
+    const response : any= await wetrocloud.queryCollection({
+      collection_id: collection_id,
+      request_query: "What is the main topic of the article?",
+      // Optional parameters:
+      // json_schema: { topic: "", description: "" },
+      // model: "gpt-4",
+    });
+
+    console.log("Streaming response:");
+    for await (const chunk of response) {
+      console.log(chunk);
+    }
+
+    // console.log("Query response:", response);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
+
+// Run the async function
+run();
